e607c1f633761a324eb0bb8d28db129cabe4e69b
Roger Wolf <roger.wolf@kit.edu>
2023-08-04 20:18:20 +0200

    adding lieterature values

---
+++ Lichtgeschwindigkeit/params/parameters_Literatur.py
@@ -0,0 +1,10 @@
+"""
+Literaturwerte für den Versuch Lichtgeschwindigkeit.
+"""
+
+Lit_speed_of_light = 2.99792458E8      # Lichtgeschwindigkeit (in m/s)
+Lit_n_air          = 1.000 292         # Brechungsindex von Luft (unter Normbedingungen)
+Lit_n_water        = 1.000 292         # Brechungsindex von Wasser (unter Normbedingungen)
+Lit_n_silicon      = 1.4               # Brechungsindex von Silikonoel
+
+__version__ = 1.0
