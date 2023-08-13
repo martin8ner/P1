b225e258a7148df51abe531744ed40627e6e4683
Roger Wolf <roger.wolf@kit.edu>
2023-08-13 20:14:04 +0200

    updating variable names

--- Geometrische_Optik/params/parameters_Literatur.py
+++ Geometrische_Optik/params/parameters_Literatur.py
@@ -3,6 +3,6 @@ Literaturwerte für den Versuch Geometrische Optik.
 """
 
 Lit_f1 = 10.      # Brennweite für Linse 1 des Zweilinsensystems (in cm)
-Lit_f1 = 20.      # Brennweite für Linse 2 des Zweilinsensystems (in cm)
+Lit_f2 = 20.      # Brennweite für Linse 2 des Zweilinsensystems (in cm)
 
 __version__ = 1.0
