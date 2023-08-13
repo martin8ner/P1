b96c7bff82f103a44ced4ed3e93cfe574c2e8077
Roger Wolf <roger.wolf@kit.edu>
2023-08-13 20:01:52 +0200

    equipping params with literature values

---
+++ Geometrische_Optik/params/parameters_Literatur.py
@@ -0,0 +1,8 @@
+"""
+Literaturwerte für den Versuch Geometrische Optik.
+"""
+
+Lit_f1 = 10.      # Brennweite für Linse 1 des Zweilinsensystems (in cm)
+Lit_f1 = 20.      # Brennweite für Linse 2 des Zweilinsensystems (in cm)
+
+__version__ = 1.0
